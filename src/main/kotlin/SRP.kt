e42060c3254951088a4c5f2282d153971f38e490
Arfan Siregar <arfan.siregar@kvendor.id>
2023-09-23 17:36:24 +0700

    Creating sample single responsibility principle

---
+++ src/main/kotlin/SRP.kt
@@ -0,0 +1,49 @@
+
+/**
+ * @author : Arfan Siregar
+ *
+ * SRP is Single Responsibility Principle
+ * */
+
+
+/**
+ * Bad practice.
+ *
+ * Where the class have then more one responsibility
+ * */
+class BadTransactionService(
+    private val id: Int?,
+) {
+    fun fetchInvoice() {
+        if (!validateInvoice()) {
+            // fetch invoice
+        }
+    }
+
+    private fun validateInvoice(): Boolean {
+        return id != null
+    }
+}
+
+
+/**
+ * Best practice.
+ *
+ * Where the class have one responsibility
+ * */
+
+class GoodTransactionService(
+    private val id: Int?,
+) {
+    fun fetchInvoice() {
+        if (Validate.invoice(id)) {
+            // fetch invoice
+        }
+    }
+}
+
+object Validate {
+    fun invoice(id: Int?): Boolean {
+        return id != null
+    }
+}
